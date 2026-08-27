12dd9bcf43246a82512390ebea99a7a95d737947
Priyanshu Pal <98373133+pryshu29@users.noreply.github.com>
2026-08-27 10:58:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,24 @@
 from pymongo import MongoClient
-
 from config import MONGODB_URI
 
-
 client = MongoClient(MONGODB_URI)
+db = client["job_agent"]
 
-database = client["job_agent"]
-
-users_collection = database["users"]
-
+users = db["users"]
 
 def check_database_connection():
     client.admin.command("ping")
     return True
+
+def save_preferences(user_id, data):
+    users.update_one(
+        {"telegram_user_id": user_id},
+        {
+            "$set": {
+                "telegram_user_id": user_id,
+                "job_preferences": data,
+                "onboarding_completed": True,
+            }
+        },
+        upsert=True,
+    )
